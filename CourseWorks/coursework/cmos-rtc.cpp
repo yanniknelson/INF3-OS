a8e5281da22d3faf5eaa2a6aed639fdacdd5db41
yanniknelson <yannikdanielnelson@gmail.com>
2021-01-20 04:10:47 +0000

    Written faster alternate method

--- CourseWorks/coursework/cmos-rtc.cpp
+++ CourseWorks/coursework/cmos-rtc.cpp
@@ -49,12 +49,26 @@ private:
 	 * @param offset The offset of the desired data
 	 * @return The date stored in the CMOS at the provided offset 
 	 */
-	uint8_t readCMOS(uint8_t offset)
+	uint8_t readCMOSOffset(uint8_t offset)
 	{
 		//specify CMOS offset
 		__outb(CMOSOFFSET, offset);
 		//get and return the data from that offset
 		return __inb(CMOSDATA);
+	}
+
+	/**
+	 * Gets all the time and data data from the CMOS, with no concern for validity
+	 * and stores it in the passed in RTCTimePoint
+	 * @param tp The RTCTimePoint to be populated with data from the CMOS
+	 */
+	void read_CMOSTime(RTCTimePoint &tp) {
+		tp.seconds = readCMOSOffset(secs);
+		tp.minutes = readCMOSOffset(mins);
+		tp.hours = readCMOSOffset(hrs);
+		tp.day_of_month = readCMOSOffset(day);
+		tp.month = readCMOSOffset(mnth);
+		tp.year = readCMOSOffset(yr);
 	}
 
 public:
@@ -74,41 +88,66 @@ public:
 	{
 		// FILL IN THIS METHOD - WRITE HELPER METHODS IF NECESSARY
 
-		//flag to indicate the update bit has been set
-		bool set = false;
-		//flag to indicate the update flag has been unset
-		bool unset = false;
+		//flag to indicate the first read has occured
+		bool first_read = false;
+		//flag to indicate the last attempted read was successful
+		bool last_success = false;
 		//status register A
 		uint8_t statA;
 		//ignore interupts
 		UniqueIRQLock l;
-		//get the A status register and wait until an update has started (bit 7 is 1)
-		//once it's started wait for it to end (bit 7 is 0)
-		//end the loop
-		do
-		{
-			statA = readCMOS(0xA);
-			if (statA & 0x80)
+		//status register B
+		uint8_t statB;
+		//stores the data from the last read
+		uint8_t last_seconds;
+		uint8_t last_minutes;
+		uint8_t last_hours;
+		uint8_t last_day_of_month;
+		uint8_t last_month;
+		uint8_t last_year;
+		//until we have successfully read the data
+		do {
+			//get the A register and only if an update is not in progress read the data
+			//this doesn't ensure an update doesn't start in the middle of the reads though
+			statA = readCMOSOffset(0x0A);
+			if (!(statA & 0x80))
 			{
-				set = true;
+				read_CMOSTime(tp);
+				first_read = true;
 			}
-			else
-			{
-				if (set)
-				{
-					unset = true;
-				}
+		} while (!first_read);
+
+		//once we have read once, we read again (ensureing we don't read on an update) 
+		//and ensure the two reads are the same, if so then the data is valid, if not
+		//then we must repeat holding the most recent read for comparison
+		do {
+			//store the last read in the appropriate variables
+			last_seconds = tp.seconds;
+			last_minutes = tp.minutes;
+			last_hours = tp.hours;
+			last_day_of_month = tp.day_of_month;
+			last_month = tp.month;
+			last_year = tp.year;
+
+			//again ensure the update bit isn't set, if not, read the data again (and the B resgister)
+			statA = readCMOSOffset(0x0A);
+			if (!(statA & 0x80))
+			{	
+				//we must also get the B register to ensure we can properly decode the new data
+				statB = readCMOSOffset(0x0B);
+				read_CMOSTime(tp);
+				//we must also ensure we flag when the new read attempt occured during an update
+				//this is because in this case the contents of tp is not updated and so by neccessity
+				//our last and new will be equal, in this case we must not use the data
+				last_success = true;
+			} else {
+				last_success = false;
 			}
-		} while (!unset);
-		//get the B status register to get register format (bit 3)
-		uint8_t statB = readCMOS(0x0B);
-		//get all of the data from the CMOS
-		tp.seconds = readCMOS(secs);
-		tp.minutes = readCMOS(mins);
-		tp.hours = readCMOS(hrs);
-		tp.day_of_month = readCMOS(day);
-		tp.month = readCMOS(mnth);
-		tp.year = readCMOS(yr);
+		//so we exit the check once all the data is the same, and the last read attempt was successful
+		} while (((last_seconds != tp.seconds) || (last_minutes != tp.minutes) || 
+		(last_hours != tp.hours) || (last_day_of_month != tp.day_of_month) || 
+		(last_month != tp.month) || (last_year != tp.year)) && last_success);	
+		
 		//if the data is BCD (bit 3 is 0) then decode and restore the data now stored in tp
 		if (!(statB & 0x04))
 		{
